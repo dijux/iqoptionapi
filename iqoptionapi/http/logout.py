0224bd375fd135b17a91bc474e9eddd034c0c39a
dijux <dijux77@gmail.com>
2020-06-26 23:00:00 +0300

    changing domain to tr-iqoption

--- iqoptionapi/http/logout.py
+++ iqoptionapi/http/logout.py
@@ -14,7 +14,7 @@ class Logout(Resource):
 
         :returns: The instance of :class:`requests.Response`.
         """
-        return self.api.send_http_request_v2(method="POST", url="https://auth.iqoption.com/api/v1.0/logout",data=data, headers=headers)
+        return self.api.send_http_request_v2(method="POST", url="https://auth.tr-iqoption.cpm/api/v1.0/logout",data=data, headers=headers)
 
     def __call__(self):
        
